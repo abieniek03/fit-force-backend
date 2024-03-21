b3b04c7021938b46eabbbdcd2914aee3e699272c
Adrian Bieniek <kontakt@abieniek.dev>
2024-03-21 12:45:46 +0100

    Add global transform response interceptor

---
+++ src/interceptors/transform-response.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformResponseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((response) => {
+        if (!response) {
+          return {
+            data: [],
+          };
+        }
+        return { data: response };
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ClerkAuthorizationGuard } from './modules/authorization/clerk-authorization.guard';
+import { TransformResponseInterceptor } from './interceptors/transform-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new ClerkAuthorizationGuard());
+  app.useGlobalInterceptors(new TransformResponseInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('FitForce API')
